bad2c41acf08ef30dc0e109cd6ead2d03ac1441b
billy4479 <giachi.ellero@gmail.com>
2026-05-21 18:08:15 +0200

    add nix package

--- flake.nix
+++ flake.nix
@@ -13,16 +13,64 @@
     flake-utils.lib.eachDefaultSystem (
       system:
       let
+        lib = nixpkgs.lib;
         pkgs = nixpkgs.legacyPackages.${system};
-        src = pkgs.lib.cleanSource ./.;
+
         pyPkgs = pkgs.python3.withPackages (
           python-pkgs: with python-pkgs; [
             python-lsp-server
             python-lsp-ruff
           ]
         );
+
+        pokemon-showdown-wrapper = pkgs.stdenv.mkDerivation (finalAttrs: {
+          pname = "pokemon-showdown-wrapper";
+          version = "1.0.0";
+
+          src = pkgs.nix-gitignore.gitignoreSource [ ] ./.;
+
+          pnpmDeps = pkgs.fetchPnpmDeps {
+            inherit (finalAttrs) src;
+            pname = finalAttrs.pname;
+            fetcherVersion = 3;
+            hash = "sha256-FEopSW6Ty6vXC+2mqf2LvyUIj8pGo3m9trujIhXwBN0=";
+          };
+
+          nativeBuildInputs = with pkgs; [
+            nodejs_latest
+            pnpm
+            pnpmConfigHook
+            makeWrapper
+          ];
+
+          buildPhase = ''
+            pnpm build
+          '';
+
+          installPhase = ''
+            mkdir -p $out/share/pokemon-showdown-wrapper
+
+            # Copy compiled JS
+            cp -r dist package.json pnpm-lock.yaml pnpm-workspace.yaml $out/share/pokemon-showdown-wrapper
+
+            mkdir -p $out/bin
+            makeWrapper ${pkgs.nodejs_latest}/bin/node $out/bin/list-allowed-moves \
+              --add-flags "$out/share/pokemon-showdown-wrapper/dist/list-allowed-moves.js"
+            makeWrapper ${pkgs.nodejs_latest}/bin/node $out/bin/showdown-wrapper \
+              --add-flags "$out/share/pokemon-showdown-wrapper/dist/showdown-wrapper.js"
+
+            cd $out/share/pokemon-showdown-wrapper
+            pnpm install --prod --offline --frozen-lockfile --node-linker=hoisted
+            rm package.json pnpm-lock.yaml pnpm-workspace.yaml
+          '';
+
+          meta.mainProgram = "showdown-wrapper";
+        });
       in
       {
+        packages.default = pokemon-showdown-wrapper;
+        packages.pokemon-showdown-wrapper = pokemon-showdown-wrapper;
+
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
             typescript-language-server
@@ -33,6 +81,8 @@
             pnpm
             pyPkgs
           ];
+
+          inputsFrom = [ pokemon-showdown-wrapper ];
         };
       }
     );
